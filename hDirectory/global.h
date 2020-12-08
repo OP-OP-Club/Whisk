8bb279aad75061d839bbe7f3a155b3164892bf5d
David Yappeter <davidyappeter@pmberjaya.com>
2020-12-08 10:24:05 +0700

    Add Global Things

---
+++ hDirectory/global.h
@@ -0,0 +1,27 @@
+#ifndef GLOBAL_PARAM
+#define GLOBAL_PARAM 1
+
+//DO NOT TOUCH!!! 
+//Boleh Lapor Terlebih dahulu jika melihat kekurangan atau ingin memberi saran tambahan
+//SEMUA TEMPAT UNTUK MENYIMPAN VARIABEL GLOBAL
+struct Recipe {
+	char *description;
+	struct Instructions *instruct_head, *instruct_tail;
+	struct Ingredient *ingredient_head, *ingredient_tail;
+	
+	struct Recipe *next, *prev;
+};
+
+struct Instructions {
+	char *steps;
+	
+	struct Instructions *next, *prev;
+};
+
+struct Ingredient {
+	char *name;
+	
+	struct Ingredient *next, *prev;
+};
+
+#endif
